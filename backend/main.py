f8346f0923138ad2767ae1882331d86d189615b7
Anusha Ranee Ravindren <anusha@student.gamudaai.academy>
2026-06-28 15:24:08 +0800

    Fix endpoint logic, add port 5174 to CORS, remove diagnostics

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app.add_middleware( ### to let frntend cakap dgn backend
   CORSMiddleware,
-  allow_origins=["http://localhost:5173"],
+  allow_origins=["http://localhost:5173", "http://localhost:5174"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"]
